69a0597084dc91d70d418be32eef71b75f47e0f1
ww2777-cpu <ww2777@columbia.edu>
2026-02-20 22:31:04 -0500

    try_1

---
+++ notebooks_Classwork4_ww2777.py
@@ -0,0 +1,35 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "python",
+      "display_name": "Python (Pyodide)",
+      "language": "python"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "python",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.8"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "b880b7d6-86eb-4ff8-af77-8e14a5c92410",
+      "cell_type": "code",
+      "source": "def WhoAmI():\n    return('ww2777')",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": 3
+    }
+  ]
+}
