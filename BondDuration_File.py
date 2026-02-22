67cf2024df69ffd5df038882cf058c2cd67297fe
ww2777-cpu <ww2777@columbia.edu>
2026-02-22 18:25:21 -0500

    Update_BondDuration & BondPrice

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,15 @@
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    periods = m * ppy
+    cf = face * couponRate/ppy
+    pvsum=0
+    wtsum=0
+    for i in range(1,periods+1):
+        discount=(1+y/ppy)**(-i)
+        pvsum += cf * discount
+        wtsum += cf * discount * i
+    pvsum += face * (1+y/ppy)**(-periods)
+    wtsum += face * (1+y/ppy)**(-periods) * periods
+    return((wtsum/pvsum)/ppy)
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    
+
--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,4 +1,4 @@
-def getBondPrice(y, face, couponRate, m, ppy):
+def getBondPrice(y, face, couponRate, m, ppy=1):
     periods=m*ppy
     cf=face*couponRate/ppy
     pvsum=0
