7ee189b4a7a2dcf2361e03d810b21ee52444fcf4
ww2777-cpu <ww2777@columbia.edu>
2026-02-22 22:02:28 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,9 @@
-
-
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    cf=face*couponRate
+    pvsum=0
+    for x,y in zip(times, yc):
+        discount=(1+y)**(-x)
+        pvsum += cf*discount
+    pvsum += face*(1+yc[-1])**(-times[-1])
+    return(pvsum)
+
