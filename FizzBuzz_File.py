22402e7d318b7049248bf80289ba8896d4c0955e
ww2777-cpu <ww2777@columbia.edu>
2026-02-22 22:44:44 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,13 @@
-
-
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 424, 'fizzbuzz']
-    return(v)
+    outlist = []
+    for i in range(start, finish+1):
+        if i%3 == 0 and i%5 == 0:
+            outlist.append("fizzbuzz")
+        elif i%5 == 0:
+            outlist.append("buzz")
+        elif i%3 == 0:
+            outlist.append("fizz")
+        else:
+            outlist.append(i)
+    return(outlist)
+
