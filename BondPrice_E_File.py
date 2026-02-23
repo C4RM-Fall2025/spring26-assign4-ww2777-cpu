c28a67e0fb07b650fc7ba860c843febf350b752a
ww2777-cpu <ww2777@columbia.edu>
2026-02-22 19:05:59 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -5,6 +5,6 @@ def getBondPrice_E(face, couponRate, m, yc):
     for t, rate in enumerate(ycc):
         discount=(1+rate)**(-(t+1))
         pvsum += cf*discount
-    pvsum += face*(1+ycc[-1])**(-len(ycc))
+    pvsum += face*(1+ycc[-1])**(-m)
     return(pvsum)
 
